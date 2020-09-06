f32f623a129e74f0d9963011ab4a68443a3d1163
Gonzalo <gonzape93@gmail.com>
2020-09-06 06:53:26 -0400

    Con animales nadadores

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+/*
         Animal willy=new Leon();
         Zoo.hacerShow(willy);
 
@@ -9,15 +9,21 @@ public class Main {
 
         Animal donatelo= new Tortuga();
         Zoo.hacerShow(donatelo);
-
-        Animal dori=new Ballena();
-        Acuario.hacerShowAgua((AnimalNadador) dori);
+*/
 
         AnimalNadador dolp=new Delfin();
         Acuario.hacerShowAgua(dolp);
 
+
+        Animal dori=new Ballena();
+        Acuario.hacerShowAgua((AnimalNadador) dori);
+
         Animal misi=new Gato();
         Acuario.hacerShowAgua((AnimalNadador) misi);
+
+
+
+
 
     }
 }
